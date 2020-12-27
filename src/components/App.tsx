04c49ee96ff4bd52c6bb55be7a1f5d7f96fe0016
Nam Nguyen <namnd86@gmail.com>
2020-12-27 13:19:48 +1100

    eslint init

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = (): JSX.Element => {
+    return (
+        <div className="app">
+            Hello Hahahaha
+        </div>
+    )
+}
+
+export default App
